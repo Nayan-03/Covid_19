2e3729a84594eaa6c7a88029e92a9642e6d39ef7
Nayan-03 <nayanmoradiya127@gmail.com>
2023-09-11 14:52:21 +0530

    doctor_page_complete

--- lib/doctor.dart
+++ lib/doctor.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:covid_19/model/doctor_model.dart';
+import 'package:covid_19/widget/doctor_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Doctorpage extends StatefulWidget {
@@ -10,99 +15,135 @@ class Doctorpage extends StatefulWidget {
 
 class _DoctorpageState extends State<Doctorpage> {
   final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    doctorData();
+  }
+
+  doctorData() async {
+    final doctorjson = await rootBundle.loadString("assets/files/doctor.json");
+    final decodedData = jsonDecode(doctorjson);
+    var doctorData = decodedData["doctor"];
+    DoctorModel.doctor = List.from(doctorData)
+        .map<Doctor>((doctor) => Doctor.formMap(doctor))
+        .toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 38.0, left: 27.0),
-                  child: Container(
-                    height: 40.0,
-                    width: 40.0,
-                    decoration: BoxDecoration(
-                      boxShadow: const [
-                        BoxShadow(
-                          blurRadius: 1.0,
-                          color: Color.fromRGBO(0, 0, 0, 0.15),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 38.0, left: 27.0),
+                    child: Container(
+                      height: 40.0,
+                      width: 40.0,
+                      decoration: BoxDecoration(
+                        boxShadow: const [
+                          BoxShadow(
+                            blurRadius: 1.0,
+                            color: Color.fromRGBO(0, 0, 0, 0.15),
+                          ),
+                        ],
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.arrow_back_sharp,
+                          color: Color.fromRGBO(34, 43, 69, 1),
                         ),
-                      ],
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.arrow_back_sharp,
-                        color: Color.fromRGBO(34, 43, 69, 1),
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 48.0, left: 105.0),
-                  child: Text(
-                    "Doctors",
-                    style: GoogleFonts.poppins(
-                      fontSize: 18.0,
-                      color: const Color.fromRGBO(34, 43, 69, 1),
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 38.0, left: 105.0),
-                  child: Container(
-                    height: 40.0,
-                    width: 40.0,
-                    decoration: BoxDecoration(
-                      boxShadow: const [
-                        BoxShadow(
-                          blurRadius: 1.0,
-                          color: Color.fromRGBO(0, 0, 0, 0.15),
-                        ),
-                      ],
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.arrow_back_sharp,
-                        color: Color.fromRGBO(34, 43, 69, 1),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 48.0, left: 105.0),
+                    child: Text(
+                      "Doctors",
+                      style: GoogleFonts.poppins(
+                        fontSize: 18.0,
+                        color: const Color.fromRGBO(34, 43, 69, 1),
+                        fontWeight: FontWeight.w400,
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 27.0, top: 28.0),
-              child: Container(
-                width: 360.0,
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: TextField(
-                  controller: _searchController,
-                  decoration: InputDecoration(
-                    hintText: 'Search for docyors',
-                    prefixIcon: IconButton(
-                      icon: const Icon(Icons.search),
-                      onPressed: () {},
+                  Padding(
+                    padding: const EdgeInsets.only(top: 38.0, left: 105.0),
+                    child: Container(
+                      height: 40.0,
+                      width: 40.0,
+                      decoration: BoxDecoration(
+                        boxShadow: const [
+                          BoxShadow(
+                            blurRadius: 1.0,
+                            color: Color.fromRGBO(0, 0, 0, 0.15),
+                          ),
+                        ],
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.arrow_back_sharp,
+                          color: Color.fromRGBO(34, 43, 69, 1),
+                        ),
+                      ),
                     ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 27.0, top: 28.0),
+                child: Container(
+                  width: 360.0,
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: TextField(
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                      hintText: 'Search for doctors',
+                      prefixIcon: IconButton(
+                        icon: const Icon(Icons.search),
+                        onPressed: () {},
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 20.0, left: 27.0, right: 27.0, bottom: 20.0),
+                child: GridView.builder(
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    itemCount: DoctorModel.doctor.length,
+                    shrinkWrap: true,
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 20.0,
+                      mainAxisSpacing: 20.0,
+                      mainAxisExtent: 199.0,
+                    ),
+                    itemBuilder: (context, index) {
+                      return DoctorWidget(doctor: DoctorModel.doctor[index]);
+                    }),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/widget/specialty_widget.dart
+++ lib/widget/specialty_widget.dart
@@ -13,79 +13,75 @@ class SpecialtyWidget extends StatefulWidget {
 class _SpecialtyWidgetState extends State<SpecialtyWidget> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(
-            right: 20.0,
-          ),
-          child: Container(
-            height: 133.0,
-            width: 114.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(25.0),
-              color: const Color.fromRGBO(107, 119, 154, 0.05),
+    return Padding(
+      padding: const EdgeInsets.only(
+        right: 20.0,
+      ),
+      child: Container(
+        height: 133.0,
+        width: 114.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25.0),
+          color: const Color.fromRGBO(107, 119, 154, 0.05),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 14.0,
+                top: 20.0,
+              ),
+              child: ListTile(
+                leading: Image.asset(
+                  widget.specialty.image,
+                  height: 55.0,
+                  width: 55.0,
+                ),
+              ),
             ),
-            child: Column(
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Text(
+                  widget.specialty.name,
+                  style: GoogleFonts.poppins(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w400,
+                    color: const Color.fromRGBO(34, 43, 69, 1),
+                  ),
+                ),
+              ),
+            ),
+            Row(
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(
-                    left: 14.0,
-                    top: 20.0,
-                  ),
-                  child: ListTile(
-                    leading: Image.asset(
-                      widget.specialty.image,
-                      height: 55.0,
-                      width: 55.0,
+                  padding: const EdgeInsets.only(left: 23.0),
+                  child: Text(
+                    widget.specialty.num,
+                    style: GoogleFonts.poppins(
+                      fontSize: 10.0,
+                      fontWeight: FontWeight.w400,
+                      color: const Color.fromRGBO(107, 119, 154, 1),
                     ),
                   ),
                 ),
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 8.0),
-                    child: Text(
-                      widget.specialty.name,
-                      style: GoogleFonts.poppins(
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.w400,
-                        color: const Color.fromRGBO(34, 43, 69, 1),
-                      ),
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.only(right: 23.0),
+                  child: Text(
+                    widget.specialty.occupation,
+                    style: GoogleFonts.poppins(
+                      fontSize: 10.0,
+                      fontWeight: FontWeight.w400,
+                      color: const Color.fromRGBO(107, 119, 154, 1),
                     ),
                   ),
-                ),
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 23.0),
-                      child: Text(
-                        widget.specialty.num,
-                        style: GoogleFonts.poppins(
-                          fontSize: 10.0,
-                          fontWeight: FontWeight.w400,
-                          color: const Color.fromRGBO(107, 119, 154, 1),
-                        ),
-                      ),
-                    ),
-                    const Spacer(),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 23.0),
-                      child: Text(
-                        widget.specialty.occupation,
-                        style: GoogleFonts.poppins(
-                          fontSize: 10.0,
-                          fontWeight: FontWeight.w400,
-                          color: const Color.fromRGBO(107, 119, 154, 1),
-                        ),
-                      ),
-                    ),
-                  ],
                 ),
               ],
             ),
-          ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
